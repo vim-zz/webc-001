df9d29307c0f5e6d203c53fb49aac21ae6176514
ofer affias <ofer.affias@gmail.com>
2022-09-14 08:24:14 +0300

    ;

--- src/ast.js
+++ src/ast.js
@@ -8,7 +8,9 @@ import { AssetManager } from "./assetManager.js";
 import { CssPrefixer } from "./css.js";
 import { AttributeSerializer } from "./attributeSerializer.js";
 import { ModuleScript } from "./moduleScript.js";
-import { Streams } from "./streams.js";
+import { Streams } from "./streams.js"
+
+
 
 class AstSerializer {
 	constructor(options = {}) {

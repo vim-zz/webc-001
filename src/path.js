384c24bb47e30de02904e91e27a28f48070055ab
ofer affias <ofer.affias@gmail.com>
2022-09-22 12:37:04 +0300

    btoj

--- src/path.js
+++ src/path.js
@@ -3,7 +3,6 @@ import path from "path";
 class Path {
 	// cross browser normalize a file path to use /
 	static normalizePath(filePath) {
-		if(typeof filePath === "string") {
 			return filePath.split(path.sep).join("/");
 		}
 		return filePath;
